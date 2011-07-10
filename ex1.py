a8e5b34d0efe42ccfdd629b9a419c61322646481
Martin Gilday <mgilday@imap.cc>
2011-07-10 21:17:14 +0100

    Did extra credit for ex1

--- ex1.py
+++ ex1.py
@@ -5,3 +5,5 @@ print "This is fun."
 print 'Yay! Printing.'
 print "I'd much rather you 'not'"
 print 'I "said" do not touch this.'
+print 'This is for extra credit!'
+# print 'This line is commented out'
